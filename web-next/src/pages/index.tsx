3d7c2e87ff60efe2528d099f16a4e60b09d65456
andreteixeira98 <andreteixeira@alu.ufc.br>
2021-03-05 23:37:37 -0300

    aula 2 - nextjs

---
+++ web-next/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import head from 'next/head';
+import { ExperienceBar } from "../components/ExperienceBar";
+
+
+export default function Home() {
+  return (
+    <div className='container'>
+    <ExperienceBar/>
+  </div>
+  )
+}
